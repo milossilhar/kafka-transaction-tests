417ae40b18368e77acfce833dbf441eab9eb518a
Miloš Šilhár <migmig095@gmail.com>
2018-11-28 11:16:59 +0100

    Scripts - new configs for zoo replica and modified starting script

--- zoo-start.sh
+++ zoo-start.sh
@@ -14,6 +14,9 @@ function printUsage {
 if [ "$#" -ne "1" -a "$#" -ne "2" ]; then
   printUsage
 fi
+if [ "$1" == "multi" -a "$#" -eq "1" ]; then
+  printUsage
+fi
 
 # Location from which this script was executed
 CMD_LOCATION=$(dirname $0)
@@ -44,13 +47,13 @@ case $1 in
   ${BINARY_LOCATION}/zookeeper-server-start.sh -daemon ${CONFIGS_LOCATION}/zookeeper_replica_03.properties && echo "INFO - Zookeeper server 3 started"
   ;;
 "multi")
-  if [ "$2" -lt "1" -a "$2" -gt "3" ]; then
+  if [ "$2" -lt "1" -o "$2" -gt "3" ]; then
     printUsage
   fi
   mkdir -p $DATA_DIR  
   echo "$2">$DATA_DIR/myid
   echo "INFO - Zookeeper replicated server $2 starting ..."
-  ${BINARY_LOCATION}/zookeeper-server-start.sh -daemon ${CONFIGS_LOCATION}/zookeeper_replica.properties && echo "INFO - Zookeeper replicated server $2 started"
+  ${BINARY_LOCATION}/zookeeper-server-start.sh -daemon ${CONFIGS_LOCATION}/zookeeper_replica_multi_0${2}.properties && echo "INFO - Zookeeper replicated server $2 started"
   ;;
 *)
   printUsage
