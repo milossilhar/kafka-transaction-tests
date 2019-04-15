07ade3caf533a0b84b315443584a0e5cb794343a
milossilhar <migmig095@gmail.com>
2019-04-15 23:43:08 +0200

    Plots - Added generating Boxplot for Size Tests

---
+++ results/size_boxplot.R
@@ -0,0 +1,18 @@
+data <- read.csv("kafka-latencies-boxplot.csv")
+
+colors <- c("aquamarine","royalblue","indianred")
+#Draw the boxplot, with the number of individuals per group
+a <- boxplot(latencies~names,
+        data=data,
+        main="Oneskorenia správ medzi producentom a odberateľom.",
+        xlab="Počet uzlov v klastri systému Kafka",
+        ylab="Oneskorenie odberateľa [ms]",
+        col=colors,
+        border="black",
+        varwidth=TRUE,
+        pch=20,
+        names=c("1", "1", "1", "3", "3", "3", "5", "5", "5", "9")
+)
+
+#text( c(1:nlevels(data$names)) , 150, paste(c("all", "one", "trans", "all", "one", "trans", "all", "one", "trans"),sep=""), col = "blue")
+legend("topleft", inset=.02, title="Nastavenie Producenta", c("ACKS=all","ACKS=1","Tranzakcie"), fill=colors, horiz=TRUE, cex=0.8)
