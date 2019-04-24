dfd7e5f941fad6b65e8d30899b67c59db3ecbdee
milossilhar <migmig095@gmail.com>
2019-04-24 10:42:46 +0200

    Plots - Added generating scripts for Size and Producer Tests

---
+++ results-producer/producer_plot_msg.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import os
+
+path = "."
+
+diffValues = 5
+
+def prod_to_index(size):
+    if (size == "1"):
+        return 0
+    if (size == "2"):
+        return 1
+    if (size == "5"):
+        return 2
+    if (size == "10"):
+        return 3
+    if (size == "50"):
+        return 4
+
+resultDict={}
+
+files = os.listdir(path)
+files.sort()
+for name in files:
+    if not name.endswith(".out"):
+        continue
+    name_replaced = name.replace("_", "-")
+    name_splitted = name_replaced.split("-")
+    if name_splitted[1] == "producer":
+        print(name)
+        producer_file = open(name, "r")
+        for line in producer_file:
+            if line.startswith("MPS - "):
+                category = "con-" + name_splitted[0] + "-" + name_splitted[2]
+                if not category in resultDict:
+                    resultDict[category]=[None]*diffValues
+                newLine = line.replace("\n", "")
+                resultDict[category][size_to_index(name_splitted[3])] = newLine.replace("MPS - ", "")
+
+CSV_FILE = "producer_plot_msg.csv"
+
+header = ",".join(resultDict.keys()) + "\n"
+maxRuns = max(list(map(len, resultDict.values())))
+for key, listValue in resultDict.items():
+    if len(listValue) < maxRuns:
+        diff = maxRuns - len(listValue)
+        resultDict[key] = listValue + [0 * i for i in range(diff)]
+
+resultFile = ""
+for i in range(maxRuns):
+    resultFile += ",".join(list(map(lambda x: str(x[i]), resultDict.values()))) + "\n"
+
+with open(CSV_FILE, "w") as csvfile:
+    csvfile.write(header)
+    csvfile.write(resultFile)
+
---
+++ results-size/size_plot_mbs.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import os
+
+path = "."
+
+def size_to_index(size):
+    if (size == "50kB"):
+        return 0
+    if (size == "100kB"):
+        return 1
+    if (size == "200kB"):
+        return 2
+    if (size == "500kB"):
+        return 3
+    if (size == "1MB"):
+        return 4
+    if (size == "5MB"):
+        return 5
+
+resultDict={}
+
+files = os.listdir(path)
+files.sort()
+for name in files:
+    if not name.endswith(".out"):
+        continue
+    name_replaced = name.replace("_", "-")
+    name_splitted = name_replaced.split("-")
+    if name_splitted[1] == "size":
+        print(name)
+        size_file = open(name, "r")
+        for line in size_file:
+            if line.startswith("BPS - "):
+                category = "con-" + name_splitted[0] + "-" + name_splitted[2]
+                if not category in resultDict:
+                    resultDict[category]=[None]*6
+                newLine = line.replace("\n", "")
+                resultDict[category][size_to_index(name_splitted[3])] = newLine.replace("BPS - ", "")
+
+CSV_FILE = "size_plot_mbs.csv"
+
+header = ",".join(resultDict.keys()) + "\n"
+maxRuns = max(list(map(len, resultDict.values())))
+for key, listValue in resultDict.items():
+    if len(listValue) < maxRuns:
+        diff = maxRuns - len(listValue)
+        resultDict[key] = listValue + [0 * i for i in range(diff)]
+
+resultFile = ""
+for i in range(maxRuns):
+    resultFile += ",".join(list(map(lambda x: str(x[i]), resultDict.values()))) + "\n"
+
+with open(CSV_FILE, "w") as csvfile:
+    csvfile.write(header)
+    csvfile.write(resultFile)
+
