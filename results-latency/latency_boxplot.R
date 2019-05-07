b7ba801e572ae6e0a021d93ff7398985c77bf84e
milossilhar <migmig095@gmail.com>
2019-05-07 11:50:13 +0200

    Plots - Latency boxplot color and axis tweaked

--- results-latency/latency_boxplot.R
+++ results-latency/latency_boxplot.R
@@ -1,5 +1,10 @@
 data <- read.csv("latency_boxplot.csv")
 
+data.aggregated <- aggregate(data$latencies, by=list(Config=data$names), FUN=mean)
+
+data.text.values <- round(unlist(data.aggregated$x), 2)
+data.text <- format(data.text.values, decimal.mark = ",")
+
 colors <- c("seagreen2","royalblue","indianred")
 colors_dark <- c("seagreen4","royalblue4","indianred4")
 #Draw the boxplot, with the number of individuals per group
@@ -10,12 +15,12 @@ a <- boxplot(latencies~names,
         ylab="Oneskorenie odberateľa [ms]",
         log="y",
         col=colors,
+        width=rep(0.5,12),
         border=colors_dark,
-        varwidth=TRUE,
         pch=20,
-        names=c("1", "1", "1", "3", "3", "3", "5", "5", "5", "9", "9", "9")
+        names=c(rep("1", 3), rep("3", 3), rep("5", 3), rep("9", 3))
 )
 
-#text( c(1:nlevels(data$names)) , 150, paste(c("all", "one", "trans", "all", "one", "trans", "all", "one", "trans"),sep=""), col = "blue")
+text( c(1:nlevels(data$names)) , 1, data.text, col = colors_dark, cex=0.8)
 legend("topleft", inset=.02, title="Nastavenie Producenta", c("ACKS=all","ACKS=1","Tranzakcie"), fill=colors, horiz=TRUE, cex=0.8)
 
