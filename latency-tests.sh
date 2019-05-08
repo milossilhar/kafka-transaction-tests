9102ef94f7abb6dd4c264a6ce817fea459966c2c
milossilhar <migmig095@gmail.com>
2019-05-08 08:31:18 +0200

    Scripts - Testing running script moving results

--- latency-tests.sh
+++ latency-tests.sh
@@ -7,20 +7,16 @@ SCRIPT="run-tests.sh"
 now=$(date +"%Y-%m-%d-%H-%M")
 
 # Run Latency Scenario with and without Same Topic Replication
-${LOCATION}/${SCRIPT} -l
+#${LOCATION}/${SCRIPT} -l
 cd ${HOME}
 mkdir latency-run-${now}
-if [ -e *latency*.out ]; then
-  mv *latency*.out ./latency-run-${now}/
-fi
+mv *latency*.out latency-run-${now}/
 cd ${LOCATION}
 
-${LOCATION}/${SCRIPT} -l --topic-same
+#${LOCATION}/${SCRIPT} -l --topic-same
 cd ${HOME}
 mkdir latency-run-same-${now}
-if [ -e *latency*.out ]; then
-  mv *latency*.out ./latency-run-same-${now}/
-fi
+mv *latency*.out latency-run-same-${now}/
 cd ${LOCATION}
 
 mailx -s "Latency Tests are Done" "migmig095@gmail.com" < email.msg
