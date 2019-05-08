a409c62477fbce545080f88de672d660c39efbc4
milossilhar <migmig095@gmail.com>
2019-05-08 08:34:35 +0200

    Scripts - mv fail silently

--- latency-tests.sh
+++ latency-tests.sh
@@ -10,13 +10,13 @@ now=$(date +"%Y-%m-%d-%H-%M")
 #${LOCATION}/${SCRIPT} -l
 cd ${HOME}
 mkdir latency-run-${now}
-mv *latency*.out latency-run-${now}/
+mv *latency*.out latency-run-${now}/ 2> /dev/null
 cd ${LOCATION}
 
 #${LOCATION}/${SCRIPT} -l --topic-same
 cd ${HOME}
 mkdir latency-run-same-${now}
-mv *latency*.out latency-run-same-${now}/
+mv *latency*.out latency-run-same-${now}/ 2> /dev/null
 cd ${LOCATION}
 
 mailx -s "Latency Tests are Done" "migmig095@gmail.com" < email.msg
