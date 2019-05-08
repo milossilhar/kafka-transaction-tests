4b17f29574f1c7696ec34e161f32e4ddea4a9eed
milossilhar <migmig095@gmail.com>
2019-05-08 08:44:32 +0200

    Scripts - Latency running scripts real test

--- latency-tests.sh
+++ latency-tests.sh
@@ -6,13 +6,13 @@ SCRIPT="run-tests.sh"
 now=$(date +"%Y-%m-%d-%H-%M")
 
 # Run Latency Scenario with and without Same Topic Replication
-#${LOCATION}/${SCRIPT} -l
+${LOCATION}/${SCRIPT} -l
 cd ${HOME}
 mkdir latency-run-${now}
 mv *latency*.out latency-run-${now}/ 2> /dev/null
 cd ${LOCATION}
 
-#${LOCATION}/${SCRIPT} -l --topic-same
+${LOCATION}/${SCRIPT} -l --topic-same
 cd ${HOME}
 mkdir latency-run-same-${now}
 mv *latency*.out latency-run-same-${now}/ 2> /dev/null
