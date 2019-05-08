0ae237660241d6898aabc1b9f6325c2b8b83f2c2
milossilhar <migmig095@gmail.com>
2019-05-08 09:39:37 +0200

    Plots - Restructuralization of script locations, latency_plot generalized to x number of runs

--- results-scripts/latency_plot.R
+++ results-scripts/latency_plot.R
@@ -2,28 +2,33 @@ data <- read.csv("latency_plot.csv")
 
 data.text <- round(unlist(data), 2)
 
-ymin <- min(data)
-ymax <- max(data) + 500
+NUMBER_OF_RUNS <- nrow(data)
 
-colors <- c("seagreen2","royalblue","indianred")
-colors.text <- rep(colors, each=3)
+ymin <- min(data)
+ymax <- max(data)
+
+colors <- c("seagreen4","royalblue","indianred")
+colors.text <- rep(colors, each=NUMBER_OF_RUNS)
+
+x.values <- c(1:NUMBER_OF_RUNS)
+axis.text <- sprintf("Beh %d.", x.values)
 
 pch_1 <- 19
 pch_3 <- 15
 pch_5 <- 17
 pch_9 <- 18
 
-pch_all <- c(rep(pch_1,3), rep(pch_3,3), rep(pch_5,3), rep(pch_9,3))
+pch_all <- c(rep(pch_1,NUMBER_OF_RUNS), rep(pch_3,NUMBER_OF_RUNS), rep(pch_5,NUMBER_OF_RUNS), rep(pch_9,NUMBER_OF_RUNS))
 pch_legend <- c(pch_1, pch_3, pch_5, pch_9)
 
 matplot(data, 
      #main="Priemerný čas oneskorenia konzumenta",
      xlab="", ylab="Priemerné oneskorenie [ms]",
-     xlim=c(1, 3), ylim=c(ymin, ymax),
+     xlim=c(1, NUMBER_OF_RUNS), ylim=c(ymin, ymax),
      type="o", pch=pch_all, lty = 1, lwd = 1.3,
      col=colors,
      xaxt="n")
-axis(1,at=c(1,2,3),labels=c("Beh 1.", "Beh 2.", "Beh 3."))
-text(c(1,2,3), data.text, data.text, pos=3, col = colors.text, cex=0.8)
+axis(1,at=x.values,labels=axis.text)
+text(x.values, data.text, data.text, pos=3, col = colors.text, cex=0.8)
 legend("topleft", inset=.02, title="Nastavenie producenta", c("ACKS=all","ACKS=1","Tranzakcie"), col=colors, horiz=FALSE, cex=0.8, lty = 1, lwd = 1.3)
 legend("topright", inset=.02, title="Veľkosť klastra", c("1", "3", "5", "9"), horiz=TRUE, cex=0.8, pch=pch_legend)
